57111770ced87abc1465ef92a56edaf6f6187cf9
amazingustav <gustavoamorim02@gmail.com>
2022-02-04 00:50:54 -0300

    Creates a simple test for order and payment creation

--- src/test/kotlin/br/com/amz/peekpay/order/OrderServiceTests.kt
+++ src/test/kotlin/br/com/amz/peekpay/order/OrderServiceTests.kt
@@ -1,13 +1,40 @@
-package br.com.amz.peekpay
+package br.com.amz.peekpay.order
 
+import br.com.amz.peekpay.createDefaultOrder
+import br.com.amz.peekpay.payment.PaymentService
+import io.mockk.every
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.MockK
+import io.mockk.junit5.MockKExtension
+import io.mockk.verify
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.boot.test.context.SpringBootTest
+import java.util.UUID
 
+@ExtendWith(MockKExtension::class)
 @SpringBootTest
-class ApplicationTests {
+class OrderServiceTests {
+
+	@InjectMockKs private lateinit var service: OrderService
+	@MockK private lateinit var repository: OrderRepository
+	@MockK private lateinit var paymentService: PaymentService
 
 	@Test
-	fun contextLoads() {
-	}
+	fun `should create an order then a payment for this order`() {
+		val order = createDefaultOrder()
 
+		every { repository.create(any()) } returns order.orderId!!
+		every { paymentService.createPayment(any(), any()) } returns UUID.randomUUID()
+
+		service.createOrderAndPay(order, 10.toBigDecimal())
+
+		verify(exactly = 1) { service.createOrder(any()) }
+		verify(exactly = 1) {
+			paymentService.createPayment(
+				eq(10.toBigDecimal()),
+				eq(order.orderId!!)
+			)
+		}
+	}
 }
